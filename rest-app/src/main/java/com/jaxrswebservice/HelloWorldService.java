d911f6c4de10d1fd941e4a6a5243bafff4102505
Juan Villa <ing.villa.jimenez@gmail.com>
2023-08-30 19:10:13 -0600

    Building Responses to HTTP Requests

--- rest-app/src/main/java/com/jaxrswebservice/HelloWorldService.java
+++ rest-app/src/main/java/com/jaxrswebservice/HelloWorldService.java
@@ -2,6 +2,9 @@ package com.jaxrswebservice;
 
 import jakarta.ws.rs.GET; // @interface GET
 import jakarta.ws.rs.Path; // @interface Path
+import jakarta.ws.rs.PathParam; // @interface PathParam
+import jakarta.ws.rs.Produces; // @interface Produces
+import jakarta.ws.rs.core.MediaType; // class MediaType
 import jakarta.ws.rs.core.Response; // abstract class Response
 
 @Path("/ws")
@@ -9,13 +12,24 @@ public class HelloWorldService {
 
     @GET
     @Path("/greeting")
-    public Response myGreeting() {
+    @Produces(MediaType.TEXT_HTML)
+    public String myGreeting() {
 
-        String message = "Welcome to the world of REST APIs in Java!";
-
-        return Response.status(200).entity(message).build();
+        return "Welcome to the world of <b>REST APIs in Java!</b>";
     }
 
+    @GET
+    @Path("/greeting/{param}")
+    public Response myGreeting(@PathParam("param") String name) {
+
+        String message = "Welcome <b>" + name + "!</b> This is the world of web services.";
+
+        return Response.status(200)
+                .entity(message)
+                .type(MediaType.TEXT_HTML)
+                .build();
+
+    }
 }
 
 // To deploy to older Tomcat versions
